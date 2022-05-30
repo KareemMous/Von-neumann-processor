e4bb4fa9153b7c14be064a9ef1d723e245f5f251
Mustafa Usama <mustafausamaa@gmail.com>
2022-05-30 05:42:12 +0200

    Update adder.vhd

--- adder.vhd
+++ adder.vhd
@@ -2,7 +2,7 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
 ENTITY adder IS
-  GENERIC (n : INTEGER := 32);
+  GENERIC (n : INTEGER := 16);
   PORT (
     D : IN STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
     val : INTEGER;
