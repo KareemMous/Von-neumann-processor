a2acba0a475b5e78e4bccc0691c05ee3801457c5
Mustafa Usama <mustafausamaa@gmail.com>
2022-05-30 05:42:34 +0200

    sp

--- spregister.vhd
+++ spregister.vhd
@@ -5,19 +5,24 @@ ENTITY spregister IS
 	GENERIC (n : INTEGER := 32);
 	PORT (
 		d : IN STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
-		enable, clk, rst : IN STD_LOGIC;
-		q : OUT STD_LOGIC_VECTOR(n - 1 DOWNTO 0) := x"000FFFFF"
+		enable : IN STD_LOGIC;
+		clk, rst : IN STD_LOGIC;
+		q : OUT STD_LOGIC_VECTOR(n - 1 DOWNTO 0) := x"000FFFFF";
+		inc : OUT INTEGER;
+		DEC : OUT INTEGER
 	);
 END spregister;
 
 ARCHITECTURE a_spregister OF spregister IS
 BEGIN
-	PROCESS (clk, rst)
+	PROCESS (clk, rst, enable)
 	BEGIN
 		IF (rst = '1') THEN
 			q <= x"000FFFFF";--2^20-1
 		ELSIF (falling_edge(clk)) AND enable = '1' THEN
 			q <= d;
+			inc <= 1;
+			dec <= - 1;
 		END IF;
 	END PROCESS;
 END a_spregister;
